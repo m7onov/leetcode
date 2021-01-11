0d0356182f6c483446f8e28db31759cc8bdb50f7
m7onov <m7onov@gmail.com>
2021-01-11 17:49:16 +0300

    save before merge

---
+++ contest_dec_2020/week_4/02_balanced_binary_tree.py
@@ -0,0 +1,37 @@
+"""
+https://leetcode.com/explore/challenge/card/december-leetcoding-challenge/572/week-4-december-22nd-december-28th/3577/
+
+Given a binary tree, determine if it is height-balanced.
+For this problem, a height-balanced binary tree is defined as:
+a binary tree in which the left and right subtrees of every node differ in height by no more than 1.
+
+Example 1:
+Input: root = [3,9,20,null,null,15,7]
+Output: true
+
+Example 2:
+Input: root = [1,2,2,3,3,null,null,4,4]
+Output: false
+
+Example 3:
+Input: root = []
+Output: true
+
+Constraints:
+The number of nodes in the tree is in the range [0, 5000].
+-10^4 <= Node.val <= 10^4
+"""
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        return str(self.val)
+
+
+def is_balanced(root: TreeNode) -> bool:
+    pass
