5da7f0f4ff0ae84a5ca7994c75d3b8d98a3bef78
Miha <m7onov@gmail.com>
2021-09-11 16:00:58 +0300

    add two numbers if linked list representation

--- array/set_matrix_zeroes.py
+++ array/set_matrix_zeroes.py
@@ -5,12 +5,16 @@ Given an m x n integer matrix matrix, if an element is 0, set its entire row and
 You must do it in place.
 
 Example 1:
+
 .. image:: https://assets.leetcode.com/uploads/2020/08/17/mat1.jpg
+
 Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
 Output: [[1,0,1],[0,0,0],[1,0,1]]
 
 Example 2:
+
 .. image:: https://assets.leetcode.com/uploads/2020/08/17/mat2.jpg
+
 Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
 Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
 
