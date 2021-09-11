e7b4e718c01085ec6431f504e0c024e5c909905e
Miha <m7onov@gmail.com>
2021-09-11 15:26:48 +0300

    length of the longest substring

--- array/longest_substring_without_repeating_chars.py
+++ array/longest_substring_without_repeating_chars.py
@@ -27,8 +27,49 @@ Constraints:
 0 <= s.length <= 5 * 10^4
 s consists of English letters, digits, symbols and spaces.
 """
+from collections import defaultdict
 
 
 class Solution:
+    # O(L) - speed
+    # O(1) - memory (string chars dictionary is limited anyway)
     def lengthOfLongestSubstring(self, s: str) -> int:
-        pass
+        m = dict()
+        max_len = 0
+        start_pos = 0
+        end_pos = None
+        for end_pos, c in enumerate(s):
+            if c in m and m[c] >= start_pos:
+                max_len = max(max_len, end_pos - start_pos)
+                start_pos = m[c] + 1
+            m[c] = end_pos
+            # print(m)
+            # print(start_pos, end_pos)
+            # print('------------------------')
+        else:
+            # print(m)
+            # print(start_pos, end_pos)
+            if end_pos is not None:
+                end_pos += 1
+            else:
+                end_pos = 0
+
+        max_len = max(max_len, end_pos - start_pos)
+        return max_len
+
+
+sol = Solution()
+res = sol.lengthOfLongestSubstring('abcabcbb')
+print(res)
+res = sol.lengthOfLongestSubstring('bbbbb')
+print(res)
+res = sol.lengthOfLongestSubstring('pwwkew')
+print(res)
+res = sol.lengthOfLongestSubstring('')
+print(res)
+res = sol.lengthOfLongestSubstring(' ')
+print(res)
+res = sol.lengthOfLongestSubstring('dvdf')
+print(res)
+res = sol.lengthOfLongestSubstring('abba')
+print(res)
