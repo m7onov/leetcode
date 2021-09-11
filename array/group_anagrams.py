7da6d36dfed10978fcff45f895e45d780e6700ad
Miha <m7onov@gmail.com>
2021-09-11 13:57:51 +0300

    group anagrams

--- array/group_anagrams.py
+++ array/group_anagrams.py
@@ -24,8 +24,67 @@ Constraints:
 strs[i] consists of lower-case English letters.
 """
 from typing import List
+from collections import defaultdict
+from random import choice, randint
+import timeit
 
 
 class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        pass
+    # O(L*W*log(W)) - speed
+    # O(L*W) - memory
+    def groupAnagramsWithSorting(self, strs: List[str]) -> List[List[str]]:
+        strs1 = sorted(strs, key=len)
+        strs2 = [''.join(sorted(s)) for s in strs1]
+        anagrams = defaultdict(list)
+        for i, s in enumerate(strs2):
+            anagrams[s].append(strs1[i])
+
+        return [v for v in anagrams.values()]
+
+    # O(L*W) - speed
+    # O(L*W) - memory
+    def groupAnagramsWithCounters(self, strs: List[str]) -> List[List[str]]:
+        anagrams = defaultdict(list)
+        for s in strs:
+            s_key = [0] * (ord('z') - ord('a') + 1)
+            for c in s:
+                s_key[ord(c) - ord('a')] += 1
+            anagrams[tuple(s_key)].append(s)
+
+        return [v for v in anagrams.values()]
+
+
+sol = Solution()
+res = sol.groupAnagramsWithSorting(["eat", "tea", "tan", "ate", "nat", "bat"])
+print(res)
+res = sol.groupAnagramsWithSorting(["", "b", ""])
+print(res)
+res = sol.groupAnagramsWithCounters(["eat", "tea", "tan", "ate", "nat", "bat"])
+print(res)
+res = sol.groupAnagramsWithCounters(["", "b", ""])
+print(res)
+
+
+def perfomance_test():
+    def generate_random_input():
+        result = []
+        d = [chr(o) for o in range(ord('a'), ord('z') + 1)]
+        for _ in range(randint(0, 10000)):
+            l = randint(0, 101)
+            random_string = ''.join(choice(d) for _ in range(l))
+            result.append(random_string)
+        return result
+
+    def perform_batch_test_with_sorting():
+        for indata in indatas:
+            Solution().groupAnagramsWithSorting(indata)
+
+    def perform_batch_test_with_counters():
+        for indata in indatas:
+            Solution().groupAnagramsWithCounters(indata)
+
+    indatas = [generate_random_input() for _ in range(100)]
+    measured_time = timeit.timeit(lambda: perform_batch_test_with_sorting(), number=10)
+    print(f'with sorting: {measured_time}')
+    measured_time = timeit.timeit(lambda: perform_batch_test_with_counters(), number=10)
+    print(f'with counters: {measured_time}')
