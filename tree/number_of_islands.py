830148e764e83fd79354c6b278615b64dd4a7adf
Miha <m7onov@gmail.com>
2021-09-25 23:09:06 +0300

    number of islands

--- tree/number_of_islands.py
+++ tree/number_of_islands.py
@@ -33,14 +33,83 @@ Constraints:
 from typing import List
 
 
-def num_islands(grid: List[List[str]]) -> int:
-    def explore_island(start_ri, start_ci):
-        grid[ri][ci] = '2'
-        pass
+def num_islands1(grid: List[List[str]]) -> int:
+    num_rows = len(grid)
+    num_cols = len(grid[0])
+
+    def explore(start_ri, start_ci):
+        stack = [(start_ri, start_ci)]
+        while len(stack) > 0:
+            rc = stack.pop()
+
+            if grid[rc[0]][rc[1]] == '1':
+                grid[rc[0]][rc[1]] = '2'
+            else:
+                continue
+
+            if rc[1] < num_cols - 1:
+                stack.append((rc[0], rc[1] + 1))
+            if rc[0] < num_rows - 1:
+                stack.append((rc[0] + 1, rc[1]))
+            if rc[1] > 0:
+                stack.append((rc[0], rc[1] - 1))
+            if rc[0] > 0:
+                stack.append((rc[0] - 1, rc[1]))
 
     island_counter = 0
     for ri, row in enumerate(grid):
         for ci, col in enumerate(row):
-            if grid[ri][ci] == '-1':
+            if grid[ri][ci] == '1':
                 island_counter += 1
-                explore_island(ri, ci)
+                explore(ri, ci)
+
+    return island_counter
+
+
+def num_islands2(grid: List[List[str]]) -> int:
+    num_rows = len(grid)
+    num_cols = len(grid[0])
+
+    def explore(start_ri, start_ci):
+        stack = [(start_ri, start_ci)]
+        while len(stack) > 0:
+            new_stack = []
+            for rc in stack:
+                if grid[rc[0]][rc[1]] == '1':
+                    grid[rc[0]][rc[1]] = '2'
+                    if rc[1] < num_cols - 1:
+                        new_stack.append((rc[0], rc[1] + 1))
+                    if rc[0] < num_rows - 1:
+                        new_stack.append((rc[0] + 1, rc[1]))
+                    if rc[1] > 0:
+                        new_stack.append((rc[0], rc[1] - 1))
+                    if rc[0] > 0:
+                        new_stack.append((rc[0] - 1, rc[1]))
+
+            stack = new_stack
+
+    island_counter = 0
+    for ri, row in enumerate(grid):
+        for ci, col in enumerate(row):
+            if grid[ri][ci] == '1':
+                island_counter += 1
+                explore(ri, ci)
+
+    return island_counter
+
+
+res = num_islands2([
+  ['1', '1', '1', '1', '0'],
+  ['1', '1', '0', '1', '0'],
+  ['1', '1', '0', '0', '0'],
+  ['0', '0', '0', '0', '0']
+])
+print(res)
+
+res = num_islands2([
+  ['1', '1', '0', '0', '0'],
+  ['1', '1', '0', '0', '0'],
+  ['0', '0', '1', '0', '0'],
+  ['0', '0', '0', '1', '1']
+])
+print(res)
