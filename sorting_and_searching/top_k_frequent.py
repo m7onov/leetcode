8b10568468df12d6985594516bb1f1c2c12d35af
m7onov <m7onov@gmail.com>
2021-10-15 15:28:16 +0300

    top k frequent

--- sorting_and_searching/top_k_frequent.py
+++ sorting_and_searching/top_k_frequent.py
@@ -20,31 +20,66 @@ It is guaranteed that the answer is unique.
 Follow up: Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
 """
 from typing import List
-from collections import defaultdict, deque
+from collections import defaultdict
+import heapq
+import random
 
 
-class Solution:
+class SolutionBruteForce:
     def top_k_frequent(self, nums: List[int], k: int) -> List[int]:
-        max_freq = 0
         freqs = defaultdict(int)
         for n in nums:
             freqs[n] += 1
-            if freqs[n] > max_freq:
-                max_freq = freqs[n]
+        result = [i[0] for i in sorted(freqs.items(), key=lambda x: x[1], reverse=True)]
+        return result[:k]
 
-        print(f'k = {k}, freqs = {dict(freqs)}, max_freq = {max_freq}, k / len(nums) = {k / len(nums)}')
 
-        result = []
-        for n, f in freqs.items():
-            print(f'{n:<2}: f / max_freq = {f / max_freq},    f / len(nums) = {f / len(nums)}')
-            if f / max_freq >= k / len(freqs):
-                result.append(n)
-
+class SolutionMaxHeap:
+    def top_k_frequent(self, nums: List[int], k: int) -> List[int]:
+        freqs = defaultdict(int)
+        for n in nums:
+            freqs[n] += 1
+        result = heapq.nlargest(k, freqs.items(), key=lambda x: x[1])
+        result = [i[0] for i in result]
         return result
 
 
+class SolutionQuickSelect:
+    def top_k_frequent(self, nums: List[int], k: int) -> List[int]:
+        freqs = defaultdict(int)
+        for n in nums:
+            freqs[n] += 1
+
+        keys = [f for f in freqs.keys()]
+        ln = len(keys)
+
+        def partition(start_idx, stop_idx, pivot_idx):
+            keys[stop_idx], keys[pivot_idx] = keys[pivot_idx], keys[stop_idx]
+            store_idx = start_idx
+            for i in range(start_idx, stop_idx):
+                if freqs[keys[i]] < freqs[keys[stop_idx]]:
+                    keys[i], keys[store_idx] = keys[store_idx], keys[i]
+                    store_idx += 1
+
+            keys[stop_idx], keys[store_idx] = keys[store_idx], keys[stop_idx]
+            return store_idx
+
+        def quickselect(start_idx, stop_idx, req_len):
+            pivot_idx = random.randint(start_idx, stop_idx)
+            pivot_true_idx = partition(start_idx, stop_idx, pivot_idx)
+            if pivot_true_idx == req_len:
+                return
+            elif pivot_true_idx < req_len:
+                quickselect(pivot_true_idx, stop_idx, req_len)
+            else:
+                quickselect(start_idx, pivot_true_idx, req_len)
+
+        quickselect(0, ln - 1, ln - k)
+        return keys[ln-k:]
+
+
 def tests():
-    sol = Solution()
+    sol = SolutionQuickSelect()
     # 1, 2
     res = sol.top_k_frequent([1, 1, 1, 2, 2, 3], 2)
     print(res)
@@ -61,7 +96,7 @@ def tests():
     res = sol.top_k_frequent([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], 2)
     print(res)
     print('-----------')
-    # 1, 2
+    # 1, 2, 3, 4
     res = sol.top_k_frequent([1, 2, 2, 3, 3, 3, 4, 4, 4, 4], 4)
     print(res)
     print('-----------')
