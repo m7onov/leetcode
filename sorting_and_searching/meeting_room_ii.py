c2358866f812c4dbd20125b377e9dc41f912a73c
m7onov <m7onov@gmail.com>
2021-10-21 17:43:48 +0300

    search in rotated sorted array

---
+++ sorting_and_searching/meeting_room_ii.py
@@ -0,0 +1,24 @@
+"""
+https://leetcode.com/explore/interview/card/top-interview-questions-medium/110/sorting-and-searching/805/
+
+Given an array of meeting time intervals where intervals[i] = [start_i, end_i], return the minimum number
+of conference rooms required.
+
+Example 1:
+Input: intervals = [[0,30],[5,10],[15,20]]
+Output: 2
+
+Example 2:
+Input: intervals = [[7,10],[2,4]]
+Output: 1
+
+Constraints:
+1 <= intervals.length <= 10^4
+0 <= start_i < end_i <= 10^6
+"""
+from typing import List
+
+
+class Solution:
+    def min_neeting_rooms(self, intervals: List[List[int]]) -> int:
+        pass
