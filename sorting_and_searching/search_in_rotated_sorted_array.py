dcfbcec191afcef2f2c0f1e6fab50d3293cc89e7
m7onov <m7onov@gmail.com>
2021-10-21 15:07:42 +0300

    search in rotated sorted array

--- sorting_and_searching/search_in_rotated_sorted_array.py
+++ sorting_and_searching/search_in_rotated_sorted_array.py
@@ -35,4 +35,87 @@ from typing import List
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        pass
+        if len(nums) == 0:
+            return -1
+
+        def bin_search(from_idx, to_idx) -> int:
+            while to_idx > from_idx:
+                mid_idx = (from_idx + to_idx) // 2
+                if nums[mid_idx] > target:
+                    to_idx = mid_idx
+                elif nums[mid_idx] < target:
+                    from_idx = mid_idx + 1
+                else:
+                    return mid_idx
+            return from_idx if nums[from_idx] == target else -1
+
+        start_idx = 0
+        stop_idx = len(nums) - 1
+        while stop_idx >= start_idx:
+            middle_idx = (start_idx + stop_idx) // 2
+            if nums[middle_idx] == target:
+                return middle_idx
+
+            if nums[middle_idx] < nums[start_idx]:
+                stop_idx = middle_idx
+            elif nums[middle_idx] > nums[stop_idx]:
+                start_idx = middle_idx + 1
+            else:
+                break
+
+        res = bin_search(0, start_idx - 1)
+        return res if res != -1 else bin_search(start_idx, len(nums) - 1)
+
+
+class SolutionOnePass():
+    def search(self, nums: List[int], target: int) -> int:
+        if len(nums) == 0:
+            return -1
+
+        start_idx = 0
+        end_idx = len(nums) - 1
+        while start_idx < end_idx:
+            middle_idx = (start_idx + end_idx) // 2
+
+            if nums[middle_idx] == target:
+                return middle_idx
+
+            if nums[middle_idx] >= nums[start_idx]:
+                if nums[start_idx] <= target <= nums[middle_idx]:
+                    end_idx = middle_idx
+                else:
+                    start_idx = middle_idx + 1
+            else:
+                if nums[middle_idx] < target <= nums[end_idx]:
+                    start_idx = middle_idx + 1
+                else:
+                    end_idx = middle_idx
+
+        return start_idx if nums[start_idx] == target else -1
+
+
+def tests():
+    sol = SolutionOnePass()
+    res = sol.search([1, 2, 3, 4, 5, 6, 7, 8], 8)
+    print(7 == res)
+    res = sol.search([6, 7, 8, 1, 2, 3, 4, 5], 8)
+    print(2 == res)
+    res = sol.search([1, 2, 3, 4, 5, 6, 7, 8, 9, 0], 0)
+    print(9 == res)
+    res = sol.search([1, 2], 1)
+    print(0 == res)
+    res = sol.search([2, 1], 1)
+    print(1 == res)
+    res = sol.search([2, 1], 3)
+    print(-1 == res)
+    res = sol.search([2, 3, 4, 1], 2)
+    print(0 == res)
+    res = sol.search([1], 1)
+    print(0 == res)
+    res = sol.search([1], 2)
+    print(-1 == res)
+    res = sol.search([], 2)
+    print(-1 == res)
+
+
+tests()
