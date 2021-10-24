1db6726f8f02c44a82882fbe1bace0f134d1b643
Miha <m7onov@gmail.com>
2021-10-24 20:29:34 +0300

    meeting room ii

--- sorting_and_searching/meeting_room_ii.py
+++ sorting_and_searching/meeting_room_ii.py
@@ -21,7 +21,7 @@ import heapq
 
 
 class Solution:
-    def min_neeting_rooms(self, intervals: List[List[int]]) -> int:
+    def min_meeting_rooms(self, intervals: List[List[int]]) -> int:
         intervals.sort(key=lambda x: x[0])
         rooms_counter = 0
         heap = []
@@ -37,13 +37,32 @@ class Solution:
                     heapq.heapreplace(heap, i[1])
         return rooms_counter
 
+    def min_meeting_rooms2(self, intervals: List[List[int]]) -> int:
+        beginings = sorted(map(lambda x: x[0], intervals))
+        endinds = sorted(map(lambda x: x[1], intervals))
+        min_rooms, counter = 0, 0
+        cur_b_i, cur_e_i = 0, 0
+        while cur_b_i < len(beginings):
+            while beginings[cur_b_i] >= endinds[cur_e_i]:
+                cur_e_i += 1
+                counter -= 1
+            counter += 1
+            cur_b_i += 1
+            min_rooms = max(min_rooms, counter)
+
+        return min_rooms
+
 
 def tests():
     sol = Solution()
-    res = sol.min_neeting_rooms([[0, 30], [5, 10], [15, 20]])
-    print(res)
-    res = sol.min_neeting_rooms([[7, 10], [2, 4]])
-    print(res)
+    res = sol.min_meeting_rooms2([[0, 30], [5, 10], [15, 20]])
+    print(f'2 = {res}')
+    res = sol.min_meeting_rooms2([[7, 10], [2, 4]])
+    print(f'1 = {res}')
+    res = sol.min_meeting_rooms2([[1293, 2986], [848, 3846], [4284, 5907], [4466, 4781], [518, 2918], [300, 5870]])
+    print(f'4 = {res}')
+    res = sol.min_meeting_rooms2([[2, 7]])
+    print(f'1 = {res}')
 
 
 tests()
