0f04a0d9f7ae63592a115b04e4fb4821cd6b653d
m7onov <m7onov@gmail.com>
2021-11-11 16:46:43 +0300

    search 2d matrix variant2

--- sorting_and_searching/search_2d_matrix.py
+++ sorting_and_searching/search_2d_matrix.py
@@ -39,13 +39,10 @@ class Solution:
     def search_matrix_0(self, matrix: List[List[int]], target: int) -> bool:
         num_rows = len(matrix)
         num_cols = len(matrix[0])
-
-        i, j = 0, 0
         for i in range(num_rows):
             for j in range(num_cols):
                 if matrix[i][j] == target:
                     return True
-
         return False
 
     def search_matrix_1(self, matrix: List[List[int]], target: int) -> bool:
@@ -90,8 +87,47 @@ class Solution:
         return False
 
     def search_matrix_2(self, matrix: List[List[int]], target: int) -> bool:
-        # TODO: divide matrix to 4 smaller matrices then search recursive
-        pass
+        # divide matrix to 4 smaller matrices then search recursive
+        num_rows = len(matrix)
+        num_cols = len(matrix[0])
+
+        def search_recursive(from_row_idx, to_row_idx, from_col_idx, to_col_idx) -> bool:
+            if from_row_idx == to_row_idx and from_col_idx == to_col_idx:
+                return matrix[from_row_idx][from_col_idx] == target
+
+            row_middle_idx = (from_row_idx + to_row_idx) // 2
+            col_middle_idx = (from_col_idx + to_col_idx) // 2
+
+            print(f'from_row_idx = {from_row_idx}, to_row_idx = {to_row_idx}, '
+                  f'from_col_idx = {from_col_idx}, to_col_idx = {to_col_idx}, '
+                  f'row_middle_idx = {row_middle_idx}, col_middle_idx = {col_middle_idx}')
+
+            if matrix[from_row_idx][from_col_idx] <= target <= matrix[row_middle_idx][col_middle_idx]:
+                # top left block
+                if search_recursive(from_row_idx, row_middle_idx, from_col_idx, col_middle_idx):
+                    return True
+
+            if row_middle_idx < to_row_idx and col_middle_idx < to_col_idx and \
+                    matrix[row_middle_idx + 1][col_middle_idx + 1] <= target <= matrix[to_row_idx][to_col_idx]:
+                # bottom right block
+                if search_recursive(row_middle_idx + 1, to_row_idx, col_middle_idx + 1, to_col_idx):
+                    return True
+
+            if row_middle_idx < to_row_idx and \
+                    matrix[row_middle_idx+1][from_col_idx] <= target <= matrix[to_row_idx][col_middle_idx]:
+                # bottom left block
+                if search_recursive(row_middle_idx+1, to_row_idx, from_col_idx, col_middle_idx):
+                    return True
+
+            if col_middle_idx < to_col_idx and \
+                    matrix[from_row_idx][col_middle_idx+1] <= target <= matrix[row_middle_idx][to_col_idx]:
+                # top right block
+                if search_recursive(from_row_idx, row_middle_idx, col_middle_idx+1, to_col_idx):
+                    return True
+
+            return False
+
+        return search_recursive(0, num_rows - 1, 0, num_cols - 1)
 
     def searh_matrix_3(self, matrix: List[List[int]], target: int) -> bool:
         # TODO: search space reduction
@@ -100,11 +136,11 @@ class Solution:
 
 def tests():
     sol = Solution()
-    res = sol.search_matrix_1([[1,   4,  7, 11, 15],
+    res = sol.search_matrix_2([[1,   4,  7, 11, 15],
                                [2,   5,  8, 12, 19],
                                [3,   6,  9, 16, 22],
                                [10, 13, 14, 17, 24],
-                               [18, 21, 23, 26, 30]], 9)
+                               [18, 21, 23, 26, 30]], 5)
     print(res)
 
 
