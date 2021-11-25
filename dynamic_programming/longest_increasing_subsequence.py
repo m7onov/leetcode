0f8bdfdca2f14755f5271d7aaafe15c87dc9ad2e
Miha <m7onov@gmail.com>
2021-11-25 21:23:02 +0300

    longest increasing subsequence

--- dynamic_programming/longest_increasing_subsequence.py
+++ dynamic_programming/longest_increasing_subsequence.py
@@ -25,35 +25,45 @@ Constraints:
 
 Follow up: Can you come up with an algorithm that runs in O(n log(n)) time complexity?
 """
-from typing import List
+from typing import List, Optional
+import bisect
 
 
 class Solution:
     def length_of_lis_0(self, nums: List[int]) -> int:
         nums_len = len(nums)
         lens = [0] * nums_len
-
         for i in range(nums_len):
             max_len = 0
             for j in range(i - 1, -1, -1):
                 if nums[j] < nums[i]:
                     max_len = max(max_len, lens[j])
             lens[i] = max_len + 1
-
-        print(lens)
         return max(lens)
 
+    # attempt of slight optimization by storing previous links
     def length_of_lis_1(self, nums: List[int]) -> int:
-        pass
+        pseq = []
+        for n in nums:
+            idx = bisect.bisect_left(pseq, n)
+            if idx < len(pseq):
+                pseq[idx] = n
+            else:
+                pseq.append(n)
+
+        # TODO: find out the real subsequence from pseq
+        return len(pseq)
 
 
 def tests():
     sol = Solution()
-    res = sol.length_of_lis_0([10, 9, 2, 5, 3, 7, 101, 18])
+    res = sol.length_of_lis_1([10, 9, 2, 5, 3, 7, 101, 18])
     print(res)
-    res = sol.length_of_lis_0([1, 50, 100, 20, 21, 22, 23])
+    res = sol.length_of_lis_1([1, 50, 100, 20, 21, 22, 23])
     print(res)
-    res = sol.length_of_lis_0([0, 1, 0, 3, 2, 3])
+    res = sol.length_of_lis_1([0, 1, 0, 3, 2, 3])
+    print(res)
+    res = sol.length_of_lis_1([1, 3, 6, 7, 9, 4, 10, 5, 6])
     print(res)
 
 
